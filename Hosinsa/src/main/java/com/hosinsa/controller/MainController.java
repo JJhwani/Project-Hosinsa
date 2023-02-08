4281e09fea27a080bb330bb53e2bd0a93c3d4c2f
JJhwani <jaehwani0610@gmail.com>
2023-02-08 10:37:06 +0900

    update

--- Hosinsa/src/main/java/com/hosinsa/controller/MainController.java
+++ Hosinsa/src/main/java/com/hosinsa/controller/MainController.java
@@ -1,5 +1,29 @@
 package com.hosinsa.controller;
 
-public class MainController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.hosinsa.domain.Criteria;
+import com.hosinsa.domain.PageDTO;
+import com.hosinsa.service.MainService;
+
+@Controller
+public class MainController {
+	
+	@Autowired
+	MainService service;
+	
+	@GetMapping("/")
+	public String mainPage(Criteria cri, Model model) {
+		model.addAttribute("viewList", service.getListProview());
+		model.addAttribute("bestList", service.getListBast());
+		model.addAttribute("newList", service.getListNew());
+		
+		int total = service.getTotalCount(cri);
+		model.addAttribute("pageMaker", new PageDTO(cri, total));
+		return "main";
+	}
+	
 }
