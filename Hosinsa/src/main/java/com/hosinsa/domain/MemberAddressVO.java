2e3ce009a49df2f93a08b6369c7bab15e4aa6393
JJhwani <jaehwani0610@gmail.com>
2023-02-17 18:02:39 +0900

    UPDATE

--- Hosinsa/src/main/java/com/hosinsa/domain/MemberAddressVO.java
+++ Hosinsa/src/main/java/com/hosinsa/domain/MemberAddressVO.java
@@ -3,25 +3,19 @@ package com.hosinsa.domain;
 import lombok.Data;
 /*
 CREATE TABLE HOSINSA_MEMBER_ADDRESS(
+    ADDRESS_NO    NUMBER(10,0) NOT NULL,
     ID            VARCHAR2(30) NOT NULL,
-    ZIPCODE1      VARCHAR2(200),
-    ADDRESS1      NUMBER(6),
-    ZIPCODE2      VARCHAR2(200),
-    ADDRESS2      NUMBER(6),
-    ZIPCODE3      VARCHAR2(200),
-    ADDRESS3      NUMBER(6),
-    PRIMARY KEY(ID),
+    ZIPCODE       NUMBER(5),
+    ADDRESS       VARCHAR2(200),
+    PRIMARY KEY(ADDRESS_NO),
     CONSTRAINT FK_HOSINSA_MEMBER_ADDRESS_ID FOREIGN KEY(ID) REFERENCES HOSINSA_MEMBER(ID) ON DELETE SET NULL
 );
 */
 
 @Data
 public class MemberAddressVO {
+	private Long address_no;
 	private String id;
-	private int zipcode1;
-	private String address1;
-	private int zipcode2;
-	private String address2;
-	private int zipcode3;
-	private String address3;
+	private int zipcode;
+	private String address;
 }
